c7a029c2863b4053433bc7f4753844e259a63d70
heth0218 <54405324+heth0218@users.noreply.github.com>
2020-06-17 12:52:10 +0530

    Add files via upload

---
+++ build/precache-manifest.16b7c26d2d1a425fd0c471bed9a4a092.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d6cc00b67047ea1d94e84bfa8aac7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64200eaa9c6200650aa8",
+    "url": "/static/css/main.4c449c1a.chunk.css"
+  },
+  {
+    "revision": "e6a4c9f6cdb2da2503f4",
+    "url": "/static/js/2.5d7b44b9.chunk.js"
+  },
+  {
+    "revision": "64200eaa9c6200650aa8",
+    "url": "/static/js/main.aa630407.chunk.js"
+  },
+  {
+    "revision": "6757c9bbb1e6e09f163a",
+    "url": "/static/js/runtime-main.f3601d8a.js"
+  }
+]);
